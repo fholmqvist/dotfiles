4063911722c688a1ca88fd940c64fc29bd163985
Holmqvist1990 <fredrik.d.holmqvist@gmail.com>
2022-07-08 11:22:39 +0200

    updated autostart

--- autostart.sh
+++ autostart.sh
@@ -3,6 +3,7 @@ id=`xinput list \
     | grep "Elan Touchpad" \
     | cut -d'=' -f2 \
     | cut -d'[' -f1` 
-xinput set-prop $id 306 1
-xinput set-prop $id 314 1
+xinput set-prop $id 306 1 # Touch click
+xinput set-prop $id 324 1 # Touch click
+xinput set-prop $id 314 1 # Natural scroll
 slstatus &
