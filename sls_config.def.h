0edcc2d8e556ebb4d0c00a6c8742126e317a0670
Holmqvist1990 <fredrik.d.holmqvist@gmail.com>
2022-08-05 23:09:40 +0200

    slstatus formatting

--- sls_config.def.h
+++ sls_config.def.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ cpu_perc, " CPU: %%s | ", NULL    },
-	{ ram_perc, "RAM: %%s | ",  NULL    },
-	{ datetime, "%s",           "%F %T" },
+	{ cpu_perc, " CPU: %s%% |", NULL    },
+	{ ram_perc, " RAM: %s%% |", NULL    },
+	{ datetime, " %s",          "%F %T" },
 };
