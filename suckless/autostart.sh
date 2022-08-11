1684d89902b5d82743eed08c08e757f74046b9cf
Holmqvist1990 <fredrik.d.holmqvist@gmail.com>
2022-08-11 13:55:56 +0200

    comments

--- suckless/autostart.sh
+++ suckless/autostart.sh
@@ -1,5 +1,9 @@
 # Set resolution
+
+# No longer works since xorg.conf:
 # xrandr -s 1280x720
+
+# Get these settings by running 'gtf [WIDTH] [HEIGHT] [REFRESH_RATE]'
 xrandr --newmode "1280x720_60.00"  74.48  1280 1336 1472 1664  720 721 724 746  -HSync +Vsync
 xrandr --addmode eDP1 "1280x720_60.00"
 xrandr --addmode eDP1 "1280x720_60.00"
