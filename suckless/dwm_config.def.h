fd9be1aba9bf43d91a67a3c1e87888467e204ed3
Holmqvist1990 <fredrik.d.holmqvist@gmail.com>
2022-08-25 19:51:22 +0200

    no longer need winkey when not using emacs

--- suckless/dwm_config.def.h
+++ suckless/dwm_config.def.h
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask /* Mod1Mask ALT, Mod4Mask Win */
+#define MODKEY Mod1Mask /* Mod1Mask ALT, Mod4Mask Win */
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
