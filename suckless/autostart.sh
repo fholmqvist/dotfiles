94a9207bcb05fc84b9c8104919e03eeb1630419b
fholmqvist <fredrik.d.holmqvist@gmail.com>
2024-01-19 08:21:39 +0100

    update autostart for new work device

--- suckless/autostart.sh
+++ suckless/autostart.sh
@@ -1,35 +1,43 @@
-# # ---- SETTINGS FOR PRIMARY MACHINE ----
+# ---- SETTINGS FOR PRIMARY MACHINE ----
 xrandr --output DVI-D-0 --right-of HDMI-0
 
 
 # # ---- SETTINGS FOR LAPTOP ----
-# # # Get these settings by running 'gtf [WIDTH] [HEIGHT] [REFRESH_RATE]'
-# # xrandr --newmode "1280x720_60.00"  74.48  1280 1336 1472 1664  720 721 724 746  -HSync +Vsync
-# # xrandr --addmode eDP1 "1280x720_60.00"
-# # xrandr --addmode eDP1 "1280x720_60.00"
-# # xrandr --output eDP1 --mode "1280x720_60.00"
+# # Get these settings by running 'gtf [WIDTH] [HEIGHT] [REFRESH_RATE]'
+# xrandr --newmode "1280x720_60.00"  74.48  1280 1336 1472 1664  720 721 724 746  -HSync +Vsync
+# xrandr --addmode eDP-1 "1280x720_60.00"
+# xrandr --addmode eDP-1 "1280x720_60.00"
+# xrandr --output eDP-1 --mode "1280x720_60.00"
 
-# # # ---- TOUCHPAD SETTINGS ----
+# # ---- WORK MOUSE SENSITIVITY ----
 
-# # id=`xinput list            \
-# #     | grep 'Elan Touchpad' \
-# #     | cut -d'=' -f2        \
-# #     | cut -d'[' -f1`
+# xinput set-prop 14 338 -0.6
 
-# # tapping=`xinput list-props $id  \
-# #     | grep 'Tapping Enabled'    \
-# #     | head -1                   \
-# #     | cut -d '(' -f 2           \
-# #     | cut -d ')' -f 1`
+# # ---- SWEDISH KEYBOARD ----
+# setxkbmap se
+# loadkeys se
 
-# # scrolling=`xinput list-props $id       \
-# #     | grep 'Natural Scrolling Enabled' \
-# #     | head -1                          \
-# #     | cut -d '(' -f 2                  \
-# #     | cut -d ')' -f 1`
+# # ---- TOUCHPAD SETTINGS ----
 
-# # xinput set-prop $id $tapping 1
-# # xinput set-prop $id $scrolling 1
+# id=`xinput list            \
+#     | grep 'Elan Touchpad' \
+#     | cut -d'=' -f2        \
+#     | cut -d'[' -f1`
+
+# tapping=`xinput list-props $id  \
+#     | grep 'Tapping Enabled'    \
+#     | head -1                   \
+#     | cut -d '(' -f 2           \
+#     | cut -d ')' -f 1`
+
+# scrolling=`xinput list-props $id       \
+#     | grep 'Natural Scrolling Enabled' \
+#     | head -1                          \
+#     | cut -d '(' -f 2                  \
+#     | cut -d ')' -f 1`
+
+# xinput set-prop $id $tapping 1
+xinput set-prop $id $scrolling 1
 
 
 # ---- SLSTATUS ----
